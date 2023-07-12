19ed3a602b7d1cf2a48870afea6f21641fd1273a
Maksim Falaleev <maxfalaleev1@gmail.com>
2023-07-12 13:29:34 +0300

    up

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,27 +1,27 @@
 return {
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require("bufferline").setup {
-                options = {
-                    offsets = {
-                        {
-                            filetype = "neo-tree",
-                            text = "Tree",
-                            separator = true,
-                            text_align = "left"
-                        }
-                    },
+    -- {
+    --     'akinsho/bufferline.nvim',
+    --     version = "*",
+    --     dependencies = 'nvim-tree/nvim-web-devicons',
+    --     config = function()
+    --         require("bufferline").setup {
+    --             options = {
+    --                 offsets = {
+    --                     {
+    --                         filetype = "neo-tree",
+    --                         text = "Tree",
+    --                         separator = true,
+    --                         text_align = "left"
+    --                     }
+    --                 },
 
-                    diagnostics = "nvim_lsp",
-                    separator_style = { "", "" },
-                    modified_icon = '●',
-                    show_close_icon = false,
-                    show_buffer_close_icons = false,
-                }
-            }
-        end
-    },
+    --                 diagnostics = "nvim_lsp",
+    --                 separator_style = { "", "" },
+    --                 modified_icon = '●',
+    --                 show_close_icon = false,
+    --                 show_buffer_close_icons = false,
+    --             }
+    --         }
+    --     end
+    -- },
 }
