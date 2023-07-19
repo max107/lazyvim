c607b5f0945e679c740f01f6157dd7ea9a258fa2
Maksim Falaleev <maxfalaleev1@gmail.com>
2023-07-19 19:26:35 +0300

    up

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,5 +1,15 @@
 return {
     {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
         "lewis6991/gitsigns.nvim",
         opts = {
             signs = {
