8009eea42696eeea461abf8d4b323191a862d579
Maksim Falaleev <maxfalaleev1@gmail.com>
2023-09-29 11:04:36 +0300

    up theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,4 +1,42 @@
 return {
+    -- {
+    --     "roobert/palette.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("palette").setup({
+    --             palettes = {
+    --                 -- built in colorscheme: grey
+    --                 main = "dust_dusk",
+    --                 -- built in accents: pastel, bright, dark
+    --                 accent = "pastel",
+    --                 state = "pastel",
+    --             },
+    --
+    --             italics = true,
+    --             transparent_background = true,
+    --
+    --             custom_palettes = {
+    --                 main = {
+    --                     dust_dusk = {
+    --                         color0 = "#121527",
+    --                         color1 = "#1A1E39",
+    --                         color2 = "#232A4D",
+    --                         color3 = "#3E4D89",
+    --                         color4 = "#687BBA",
+    --                         color5 = "#A4B1D6",
+    --                         color6 = "#bdbfc9",
+    --                         color7 = "#DFE5F1",
+    --                         color8 = "#e9e9ed",
+    --                     },
+    --                 },
+    --             },
+    --             accent = {},
+    --             state = {},
+    --         })
+    --         vim.cmd.colorscheme "palette"
+    --     end,
+    -- },
     {
         "catppuccin/nvim",
         name = "catppuccin",
