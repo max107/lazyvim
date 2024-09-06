55976005342d836922c740ff31059d96c22259af
Maksim Falaleev <maxfalaleev1@gmail.com>
2024-09-06 09:18:14 +0300

    disable plugins

--- init.lua
+++ init.lua
@@ -1,25 +1,39 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("opts")
 require("lazy").setup("plugins", {
-	change_detection = {
-		enabled = false,
-		notify = false,
-	},
-	ui = {
-		border = "single",
-		title = "Lazy",
-		title_pos = "left"
-	}
+    change_detection = {
+        enabled = false,
+        notify = false,
+    },
+    ui = {
+        border = "single",
+        title = "Lazy",
+        title_pos = "left"
+    },
+    defaults = {
+        -- lazy = true
+    },
+    lockfile = vim.fn.stdpath 'data' .. '/lazy-lock.json', -- hide lockfile away
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                'osc52', 'gzip', 'matchit',
+                'matchparen', 'netrwPlugin', 'tarPlugin',
+                'tohtml', 'tutor', 'zipPlugin', 'spellfile',
+                'rplugin', 'editorconfig',
+            }
+        }
+    }
 })
