c47a836bb2636af9d76e56412a90645274e87a4b
Maksim Falaleev <maxfalaleev1@gmail.com>
2025-07-14 14:07:29 +0800

    enable sonokai by default

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,55 +1,57 @@
 return {
-  -- {
-  --   "sainnhe/sonokai",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd [[
-  --           let g:sonokai_transparent_background = 1
-  --           colorscheme sonokai
-  --           ]]
-  --   end
-  -- },
-
   {
-    "f-person/auto-dark-mode.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      set_dark_mode = function()
-        vim.cmd('colorscheme github_dark')
-      end,
-      set_light_mode = function()
-        vim.cmd('colorscheme github_light_default')
-      end,
-      update_interval = 3000,
-      fallback = "dark"
-    }
-  },
-
-  {
-    "projekt0n/github-nvim-theme",
+    "sainnhe/sonokai",
     lazy = false,
     priority = 1000,
     config = function()
-      require('github-theme').setup({
-        options = {
-          transparent = true,
-        }
-      })
-
-      -- if vim.o.background == "light" then
-      --   vim.cmd('colorscheme github_light_default')
-      -- else
-      --   vim.cmd('colorscheme github_dark')
-      -- end
-
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+      vim.cmd [[
+        let g:sonokai_transparent_background = 1
+        colorscheme sonokai
+      ]]
     end
   },
+
+
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   dependencies = {
+  --     {
+  --       "f-person/auto-dark-mode.nvim",
+  --       lazy = false,
+  --       priority = 1000,
+  --       opts = {
+  --         set_dark_mode = function()
+  --           vim.cmd('colorscheme github_dark')
+  --         end,
+  --         set_light_mode = function()
+  --           vim.cmd('colorscheme github_light_default')
+  --         end,
+  --         update_interval = 3000,
+  --         fallback = "dark"
+  --       }
+  --     },
+  --   },
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('github-theme').setup({
+  --       options = {
+  --         transparent = true,
+  --       }
+  --     })
+  --
+  --     -- if vim.o.background == "light" then
+  --     --   vim.cmd('colorscheme github_light_default')
+  --     -- else
+  --     --   vim.cmd('colorscheme github_dark')
+  --     -- end
+  --
+  --     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  --     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  --     vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+  --     vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+  --   end
+  -- },
 
   -- {
   --   'akinsho/bufferline.nvim',
