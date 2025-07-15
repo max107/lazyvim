83e3f4e257c9a9bafc6a8debd81f5a5604e0fc28
Maksim Falaleev <maxfalaleev1@gmail.com>
2025-07-15 13:24:44 +0800

    fix go format

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,7 +1,7 @@
 return {
   {
     "ray-x/go.nvim",
-    dependencies = {     -- optional packages
+    dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
@@ -10,19 +10,19 @@ return {
       -- lsp_keymaps = false,
       -- other options
     },
-    config = function(_, opts)
-      require("go").setup(opts)
-      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-          require('go.format').goimports()
-        end,
-        group = format_sync_grp,
-      })
-    end,
+    -- config = function(_, opts)
+    --   require("go").setup(opts)
+    --   local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    --   vim.api.nvim_create_autocmd("BufWritePre", {
+    --     pattern = "*.go",
+    --     callback = function()
+    --       require('go.format').goimports()
+    --     end,
+    --     group = format_sync_grp,
+    --   })
+    -- end,
     event = { "CmdlineEnter" },
     ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()'     -- if you need to install/update all binaries
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   }
 }
