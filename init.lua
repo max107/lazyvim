b871d452375007e6c39d36fea92a0fe2473ffd30
Maksim Falaleev <maxfalaleev1@gmail.com>
2026-03-23 10:04:25 +0300

    drop colorful menu

--- init.lua
+++ init.lua
@@ -253,21 +253,6 @@ local plugins = {
         },
         menu = {
           auto_show = true,
-          draw = {
-            -- We don't need label_description now because label and label_description are already
-            -- combined together in label by colorful-menu.nvim.
-            columns = { { "kind_icon" }, { "label", gap = 1 } },
-            components = {
-              label = {
-                text = function(ctx)
-                  return require("colorful-menu").blink_components_text(ctx)
-                end,
-                highlight = function(ctx)
-                  return require("colorful-menu").blink_components_highlight(ctx)
-                end,
-              },
-            },
-          },
         },
       },
       sources = {
@@ -282,100 +267,8 @@ local plugins = {
     },
     opts_extend = { "sources.default" },
   },
-  {
-    "xzbdmw/colorful-menu.nvim",
-    opts = {
-      ls = {
-        lua_ls = {
-          -- Maybe you want to dim arguments a bit.
-          arguments_hl = "@comment",
-        },
-        gopls = {
-          -- By default, we render variable/function's type in the right most side,
-          -- to make them not to crowd together with the original label.
 
-          -- when true:
-          -- foo             *Foo
-          -- ast         "go/ast"
-
-          -- when false:
-          -- foo *Foo
-          -- ast "go/ast"
-          align_type_to_right = true,
-          -- When true, label for field and variable will format like "foo: Foo"
-          -- instead of go's original syntax "foo Foo". If align_type_to_right is
-          -- true, this option has no effect.
-          add_colon_before_type = false,
-          -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
-          preserve_type_when_truncate = true,
-        },
-        -- for lsp_config or typescript-tools
-        ts_ls = {
-          -- false means do not include any extra info,
-          -- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
-          extra_info_hl = "@comment",
-        },
-        vtsls = {
-          -- false means do not include any extra info,
-          -- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
-          extra_info_hl = "@comment",
-        },
-        ["rust-analyzer"] = {
-          -- Such as (as Iterator), (use std::io).
-          extra_info_hl = "@comment",
-          -- Similar to the same setting of gopls.
-          align_type_to_right = true,
-          -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
-          preserve_type_when_truncate = true,
-        },
-        clangd = {
-          -- Such as "From <stdio.h>".
-          extra_info_hl = "@comment",
-          -- Similar to the same setting of gopls.
-          align_type_to_right = true,
-          -- the hl group of leading dot of "•std::filesystem::permissions(..)"
-          import_dot_hl = "@comment",
-          -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
-          preserve_type_when_truncate = true,
-        },
-        zls = {
-          -- Similar to the same setting of gopls.
-          align_type_to_right = true,
-        },
-        roslyn = {
-          extra_info_hl = "@comment",
-        },
-        dartls = {
-          extra_info_hl = "@comment",
-        },
-        -- The same applies to pyright/pylance
-        basedpyright = {
-          -- It is usually import path such as "os"
-          extra_info_hl = "@comment",
-        },
-        pylsp = {
-          extra_info_hl = "@comment",
-          -- Dim the function argument area, which is the main
-          -- difference with pyright.
-          arguments_hl = "@comment",
-        },
-        -- If true, try to highlight "not supported" languages.
-        fallback = true,
-        -- this will be applied to label description for unsupport languages
-        fallback_extra_info_hl = "@comment",
-      },
-      -- If the built-in logic fails to find a suitable highlight group for a label,
-      -- this highlight is applied to the label.
-      fallback_highlight = "@variable",
-      -- If provided, the plugin truncates the final displayed text to
-      -- this width (measured in display cells). Any highlights that extend
-      -- beyond the truncation point are ignored. When set to a float
-      -- between 0 and 1, it'll be treated as percentage of the width of
-      -- the window: math.floor(max_width * vim.api.nvim_win_get_width(0))
-      -- Default 60.
-      max_width = 60,
-    },
-  },
+  -- golang development
   {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
@@ -402,13 +295,12 @@ local plugins = {
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
-  {
-    "nfnty/vim-nftables",
-  },
-  {
-    "terrastruct/d2-vim",
-    ft = { "d2" },
-  },
+
+  -- nftables syntax highlight
+  { "nfnty/vim-nftables" },
+
+  -- d2 diagrams syntax highlight
+  { "terrastruct/d2-vim", ft = { "d2" } },
 
   {
     "windwp/nvim-autopairs",
@@ -421,14 +313,13 @@ local plugins = {
         ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
         enable_moveright = true,
         enable_afterquote = true,
-        check_ts = false,
+        check_ts = true,
         map_bs = true,
         map_c_h = false,
         map_c_w = false,
       })
     end,
   },
-
   {
     "grafana/vim-alloy",
     config = function()
